94517201d456d334060d2efb2346607f8f9337ef
totoguri <finalyo4@naver.com>
2021-09-13 17:20:31 +0900

    commit

--- practiceC/main.c
+++ practiceC/main.c
@@ -1,5 +1,94 @@
 #include <stdio.h>
 
-int main(void) {
-	printf("Hello, git");
-}
+//int main() {
+//	printf("Hello, git");
+//	return 0;
+//}
+
+// p.50
+
+//int main() {
+//	int num1 = 3;
+//	int num2 = 4;
+//	int result = num1 + num2;
+//
+//	return 0;
+//}
+
+//int main(void) {
+//	int num1 = 10;
+//	int num2 = 12;
+//	int result1, result2, result3;
+//
+//	result1 = (num1 == num2);
+//	result2 = (num1 <= num2);
+//	result3 = (num1 > num2);
+//
+//	printf("result1: %d \n", result1);
+//	printf("result2: %d \n", result2);
+//	printf("result3: %d \n", result3);
+//	return 0;
+//}
+
+//int main() {
+//	int num1 = 15;
+//	int num2 = 20;
+//	int num3 = ~num1;
+//	printf("Result of AND: %d \n", num3);
+//	return 0;
+//}
+
+// p.134
+
+//int main() {
+//	char sel;
+//	printf("M: morning, A: afternoon, E: evening \n");
+//	printf("Type: ");
+//	scanf_s("%c", &sel);
+//
+//	switch (sel) {
+//	case 'M':
+//	case 'm':
+//		printf("Morning \n");
+//		break;
+//	case 'A':
+//	case 'a':
+//		printf("Afternoon \n");
+//		break;
+//	case 'E':
+//	case 'e':
+//		printf("Evening \n");
+//		break;
+//	}
+//	return 0;
+//}
+
+//int Add(int num1, int num2) {
+//	return num1 + num2;
+//}
+//
+//void ShowAddResult(int num) {
+//	printf("Result: %d \n", num);
+//}
+//
+//int ReadNum(void) {
+//	int num;
+//	scanf_s("%d", &num);
+//	return num;
+//}
+//
+//void HowToUseThisProg(void) {
+//	printf("Type two numbers.\n");
+//	printf("Start: ");
+//}
+//
+//int main() {
+//	int result, num1, num2;
+//	HowToUseThisProg();
+//	num1 = ReadNum();
+//	num2 = ReadNum();
+//	result = Add(num1, num2);
+//	ShowAddResult(result);
+//	return 0;
+//}
+
