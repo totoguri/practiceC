d1192a440005e49a9cc57c7dad979a9a711155db
totoguri <finalyo4@naver.com>
2021-09-14 23:54:52 +0900

    commit

--- practiceC/main.c
+++ practiceC/main.c
@@ -149,3 +149,17 @@
 //}
 
 // p.300
+
+int main() {
+	char str1[] = "My String";
+	char* str2 = "Your String";
+	printf("%s %s \n", str1, str2);
+
+	str2 = "Our String";
+	printf("%s %s \n", str1, str2);
+
+	str1[0] = 'X';
+	//str2[0] = 'X';
+	printf("%s %s \n", str1, str2);
+	return 0;
+}
