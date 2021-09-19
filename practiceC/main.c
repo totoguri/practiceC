25920a41506df1ff4e0dc87138d18af7b1eec136
totoguri <finalyo4@naver.com>
2021-09-19 12:20:11 +0900

    c

--- practiceC/main.c
+++ practiceC/main.c
@@ -700,27 +700,341 @@
 //}
 
 
-typedef struct point {
-	int xpos;
-	int ypos;
-} Point;
+//typedef struct point {
+//	int xpos;
+//	int ypos;
+//} Point;
+//
+//void ShowPosition(Point pos) {
+//	printf("[%d, %d] \n", pos.xpos, pos.ypos);
+//}
+//
+//Point GetCurrentPosition(void) {
+//	Point cen;
+//	printf("Input current pos: ");
+//	scanf_s("%d %d", &cen.xpos, &cen.ypos);
+//	return cen;
+//}
+//
+//int main(void) {
+//	Point curPos = GetCurrentPosition();
+//	ShowPosition(curPos);
+//	return 0;
+//}
 
-void ShowPosition(Point pos) {
-	printf("[%d, %d] \n", pos.xpos, pos.ypos);
-}
 
-Point GetCurrentPosition(void) {
-	Point cen;
-	printf("Input current pos: ");
-	scanf_s("%d %d", &cen.xpos, &cen.ypos);
-	return cen;
-}
+//typedef struct person {
+//	char name[20];
+//	char phoneNum[20];
+//	int age;
+//} Person;
+//
+//void ShowPersonInfo(Person man) {
+//	printf("name: %s \n", man.name);
+//	printf("phone: %s \n", man.phoneNum);
+//	printf("age: %d \n", man.age);
+//}
+//
+//Person ReadPersonInfo(void) {
+//	Person man;
+//	printf("name? "); scanf_s("%s", man.name, sizeof(man.name));
+//	printf("phone? "); scanf_s("%s", man.phoneNum, sizeof(man.name));
+//	printf("age? "); scanf_s("%d", &man.age);
+//	return man;
+//}
+//
+//int main() {
+//	Person man = ReadPersonInfo();
+//	ShowPersonInfo(man);
+//	return 0;
+//}
 
-int main(void) {
-	Point curPos = GetCurrentPosition();
-	ShowPosition(curPos);
-	return 0;
-}
+// Call-by-reference
+//typedef struct point {
+//	int xpos;
+//	int ypos;
+//} Point;
+//
+//void OrgSymTrans(Point* ptr) {
+//	ptr->xpos = (ptr->xpos) * -1;
+//	ptr->ypos = (ptr->ypos) * -1;
+//}
+//
+//void ShowPosition(Point pos) {
+//	printf("[%d, %d] \n", pos.xpos, pos.ypos);
+//}
+//
+//int main(void) {
+//	Point pos = { 7, -5 };
+//	OrgSymTrans(&pos);
+//	ShowPosition(pos);
+//	OrgSymTrans(&pos);
+//	ShowPosition(pos);
+//	return 0;
+//}
 
 
-// p.479
+//typedef struct point {
+//	int xpos;
+//	int ypos;
+//} Point;
+//
+//int main() {
+//	Point pos1 = { 1, 2 };
+//	Point pos2;
+//	pos2 = pos1;
+//
+//	printf("SIZE: %d \n", sizeof(pos1));
+//	printf("[%d, %d] \n", pos1.xpos, pos1.ypos);
+//	printf("SIZE: %d \n", sizeof(pos2));
+//	printf("[%d, %d] \n", pos2.xpos, pos2.ypos);
+//	return 0;
+//}
+
+
+//typedef struct point {
+//	int xpos;
+//	int ypos;
+//} Point;
+//
+//Point AddPoint(Point pos1, Point pos2) {
+//	Point pos = { pos1.xpos + pos2.xpos, pos1.ypos + pos2.ypos };
+//	return pos;
+//}
+//
+//Point MinPoint(Point pos1, Point pos2) {
+//	Point pos = { pos1.xpos - pos2.xpos, pos1.ypos - pos2.ypos };
+//	return pos;
+//}
+//
+//int main() {
+//	Point pos1 = { 5, 6 };
+//	Point pos2 = { 2, 9 };
+//	Point result;
+//
+//	result = AddPoint(pos1, pos2);
+//	printf("[%d, %d] \n", result.xpos, result.ypos);
+//	result = MinPoint(pos1, pos2);
+//	printf("[%d, %d] \n", result.xpos, result.ypos);
+//	return 0;
+//}
+
+
+//typedef struct student {
+//	char name[20];
+//	char stdnum[20];
+//	char school[20];
+//	char major[20];
+//	int year;
+//} Student;
+//
+//void ShowStudentInfo(Student* sptr) {
+//	printf("학생 이름: % \n", sptr->name);
+//	printf("학생 고유번호: %s \n", sptr->stdnum);
+//	printf("학교 이름: %s \n", sptr->school);
+//	printf("선택 전공: %s \n", sptr->major);
+//	printf("학년: %d \n", sptr->year);
+//}
+//
+//int main() {
+//	Student arr[7];
+//	int i;
+//
+//	for (i = 0; i < 7; i++) {
+//		printf("이름: "); scanf_s("%s", arr[i].name, sizeof(arr[i].name));
+//		printf("번호: "); scanf_s("%s", arr[i].stdnum, sizeof(arr[i].stdnum));
+//		printf("학교: "); scanf_s("%s", arr[i].school, sizeof(arr[i].school));
+//		printf("전공: "); scanf_s("%s", arr[i].major, sizeof(arr[i].major));
+//		printf("학년: "); scanf_s("%d", &arr[i].year, sizeof(arr[i].year));
+//	}
+//
+//	for (i = 0; i < 7; i++)
+//		ShowStudentInfo(&arr[i]);
+//	return 0;
+//}
+
+
+//typedef struct point {
+//	int xpos;
+//	int ypos;
+//} Point;
+//
+//typedef struct circle {
+//	Point cen;
+//	double rad;
+//} Circle;
+//
+//void ShowCircleInfo(Circle* cptr) {
+//	printf("[%d, %d] \n", (cptr->cen).xpos, (cptr->cen).ypos);
+//	printf("radius: %g \n\n", cptr->rad);
+//}
+//
+//int main() {
+//	Circle c1 = { {1, 2}, 3.5 };
+//	Circle c2 = { 2, 4, 3.9 };
+//	ShowCircleInfo(&c1);
+//	ShowCircleInfo(&c2);
+//	return 0;
+//}
+
+
+//typedef struct sbox {
+//	int mem1;
+//	int mem2;
+//	double mem3;
+//} SBox;
+//
+//typedef union ubox {
+//	int mem1;
+//	int mem2;
+//	double mem3;
+//} UBox;
+//
+//int main(void) {
+//	SBox sbx;
+//	UBox ubx;
+//	printf("%p %p %p \n", &sbx.mem1, &sbx.mem2, &sbx.mem3);
+//	printf("%p %p %p \n", &ubx.mem1, &ubx.mem2, &ubx.mem3);
+//	printf("%d %d \n", sizeof(SBox), sizeof(UBox));
+//	return 0;
+//}
+
+
+//typedef union ubox {
+//	int mem1;
+//	int mem2;
+//	double mem3;
+//} UBox;
+//
+//int main() {
+//	UBox ubx;
+//	ubx.mem1 = 20;
+//	printf("%d \n", ubx.mem2);
+//
+//	ubx.mem3 = 7.15;
+//	printf("%d \n", ubx.mem1);
+//	printf("%d \n", ubx.mem2);
+//	printf("%g \n", ubx.mem3);
+//	return 0;
+//}
+
+
+//typedef struct dbshort {
+//	unsigned short upper;
+//	unsigned short lower;
+//} DBShort;
+//
+//typedef union rdbuf {
+//	int iBuf;
+//	char bBuf[4];
+//	DBShort sBuf;
+//} RDBuf;
+//
+//int main() {
+//	RDBuf buf;
+//	printf("정수 입력: "); scanf_s("%d", &(buf.iBuf));
+//
+//	printf("상위 2바이트: %u \n", buf.sBuf.upper);
+//	printf("하위 2바이트: %u \n", buf.sBuf.lower);
+//	printf("상위 1바이트 아스키 코드: %c \n", buf.bBuf[0]);
+//	printf("하위 1바이트 아스키 코드: %c \n", buf.bBuf[3]);
+//	return 0;
+//}
+
+
+//typedef enum syllable {
+//	Do=1, Re=2, Mi=3, Fa=4, So=5, La=6, Ti=7
+//} Syllable;
+//
+//void Sound(Syllable sy) {
+//	switch (sy) {
+//	case Do:
+//		puts("Do"); return;
+//	case Re:
+//		puts("Re"); return;
+//	case Mi:
+//		puts("Mi"); return;
+//	case Fa:
+//		puts("Fa"); return;
+//	case So:
+//		puts("So"); return;
+//	case La:
+//		puts("La"); return;
+//	case Ti:
+//		puts("Ti"); return;
+//	}
+//	puts("Daaaa");
+//}
+//
+//int main() {
+//	Syllable tone;
+//	for (tone = Do; tone <= Ti; tone += 1)
+//		Sound(tone);
+//	return 0;
+//}
+
+
+//char* ReadUserName(void) {
+//	char name[30];
+//	printf("What's your name? ");
+//	gets(name);
+//	return name;
+//}
+//
+//int main() {
+//	char* name1;
+//	char* name2;
+//	name1 = ReadUserName();
+//	printf("name1: %s \n", name1);
+//	name2 = ReadUserName();
+//	printf("name2: %s \n", name2);
+//	return 0;
+//}
+
+
+//int main() {
+//	int* ptr1 = (int*)malloc(sizeof(int));
+//	int* ptr2 = (int*)malloc(sizeof(int) * 7);
+//	int i;
+//
+//	*ptr1 = 20;
+//	for (i = 0; i < 7; i++)
+//		ptr2[i] = i + i;
+//
+//	printf("%d \n", *ptr1);
+//	for (i = 0; i < 7; i++)
+//		printf("%d ", ptr2[i]);
+//
+//	free(ptr1);
+//	free(ptr2);
+//	return 0;
+//}
+
+
+//#include <stdlib.h>
+//
+//// A new malloc memory address will be created everytime this func is called.
+//char* ReadUserName(void) {
+//	char* name = (char*)malloc(sizeof(char) * 30);
+//	printf("What's your name? ");
+//	gets(name);
+//	return name;
+//}
+//
+//int main() {
+//	char* name1;
+//	char* name2;
+//	name1 = ReadUserName();
+//	printf("name1: %s \n", name1);
+//	name2 = ReadUserName();
+//	printf("name2: %s \n", name2);
+//
+//	printf("again name1: %s \n", name1);
+//	printf("again name2: %s \n", name2);
+//	free(name1);
+//	free(name2);
+//	return 0;
+//}
+
+
+// p.558
